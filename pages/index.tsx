bd52690dd8b2af07086a7c2e32123d020d6225e6
Peter Watts <pwattsmail@gmail.com>
2022-04-24 08:20:47 +0200

    add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
     <div>
       {address ? (
         <>
-          <button onClick={disconnectWallet}>Disconnect Wallet</button>
+          <button className="btn-primary-fill col-span-2 col-start-3 ml-auto md:col-span-4 md:col-start-5 lg:col-span-4 lg:col-start-9" onClick={disconnectWallet}>Disconnect Wallet</button>
           <p>Your address: {address}</p>
           <button onClick={async () => {
             if (!signer) {
